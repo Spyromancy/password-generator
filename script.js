20606956e961afba39c0dc000e93e3e93150baec
Josh <josh.haddix598@gmail.com>
2022-06-11 11:40:31 -0500

    Ensured 1 of each selected character appears in password

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ function generatePassword() {
   var uppercase = lowercase.toUpperCase(); // something something smarter not harder
   var numbers = "1234567890";
   var special = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var use_lower , use_upper , use_num , use_spec;
+  var use_lower, has_lower, use_upper, has_upper, use_num, has_num, use_spec, has_spec;
+  has_lower = has_upper = has_num = has_spec = false;
   var len;
   var password = "";
 
@@ -42,8 +43,7 @@ function generatePassword() {
     use_num=window.confirm("Include numbers?");
     use_spec=window.confirm("Include special characters?");
     if(!(use_lower || use_upper || use_num || use_spec)){
-      window.alert(`... so you want a ${len} character password populated by what? null values? Kanji? arabic? this is UTF-8 buddy pick one of the options.`) // "Please select at least one"
-      // Legal(*) has informed me that code that sasses the user is "Unprofessional" and "Will make the client complain" bunch of fun hatin' suits.
+      window.alert(`... so you want a ${len} character password populated by what? null values? Kanji? arabic? this is UTF-8 buddy pick one of the options.`) // i learned f literals in python, i'm not going back.
     }
   }while(!(use_lower || use_upper || use_num || use_spec))
 
@@ -60,26 +60,50 @@ function generatePassword() {
   if(use_spec){
     allowed.push("special");
   }
-  console.log(allowed)
+
+
   while(password.length<len){
     var char;
     var chartype= allowed[Math.floor(Math.random()*allowed.length)]; // this should give each character type roughly the same chance of inclusion, otherwise numbers will appear much less often.
-    console.log(chartype);
-    if(chartype==="lowercase"){
-      char= lowercase[Math.floor(Math.random()*lowercase.length)]
+    if(password.length+4>=len && use_lower && !has_lower){
+      char= lowercase[Math.floor(Math.random()*lowercase.length)];
+      has_lower=true;
+      password+=char;
+    }
+    if(password.length+4>=len && use_upper && !has_upper){
+      char= uppercase[Math.floor(Math.random()*uppercase.length)];
+      has_upper=true;
+      password+=char;
+    }
+    if(password.length+4>=len && use_num && !has_num){
+      char= numbers[Math.floor(Math.random()*numbers.length)];
+      has_num=true;
+      password+=char;
+    }
+    if(password.length+4>=len && use_spec && !has_spec){
+      char= special[Math.floor(Math.random()*special.length)];
+      password+=char;
+      has_spec=true;
+    }
+    else if(chartype==="lowercase"){
+      char= lowercase[Math.floor(Math.random()*lowercase.length)];
+      has_lower=true;
     }
     else if(chartype==="uppercase"){
-      char= uppercase[Math.floor(Math.random()*uppercase.length)]
+      char= uppercase[Math.floor(Math.random()*uppercase.length)];
+      has_upper=true;
     }
     else if(chartype==="numbers"){
-      char= numbers[Math.floor(Math.random()*numbers.length)]
+      char= numbers[Math.floor(Math.random()*numbers.length)];
+      has_num=true;
     }
     else if(chartype==="special"){ // Q: but couldn't this just be an 'else' statement? A: yes. but by the time i realized that i already set up the logic and instead of spending time to fix it i decided to write this comment
-      char= special[Math.floor(Math.random()*special.length)]
+      char= special[Math.floor(Math.random()*special.length)];
+      has_spec=true;
     }
-    console.log(char);
     password+=char;
   }
+  /**/
 
   return password;
 }
